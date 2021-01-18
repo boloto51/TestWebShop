66c3c9720ee78b6954b38047243a8c59c53256db
student-programming <student-programming@yandex.ru>
2021-01-18 23:04:43 +0300

    change CreateOrder.cshtml and OrderManager.ts

--- TestWebShop/TestWebShop/app/scripts/Order/OrderManager.ts
+++ TestWebShop/TestWebShop/app/scripts/Order/OrderManager.ts
@@ -2,6 +2,7 @@
 
 export class OrderManager
 {
+    goods: { id: number, count: number, price: number }[] = [];
 
     constructor()
     {
@@ -12,7 +13,41 @@ export class OrderManager
         $(".goodnumber-input").on("input", ev => {
             const target = $(ev.target);
             const goodId = target.data("goodid");
-            alert(goodId);
+
+            if (Number(target.val()) > 0) {
+                $(".goddnumber-add-" + goodId).show();
+            }
+            else {
+                $(".goddnumber-add-" + goodId).hide();
+            }
+        })
+
+        $(".goodnumber-add").on("click", ev => {
+            const target = $(ev.target);
+            const goodId = target.data("goodid");
+            const elem = $(".goddnumber-input-" + goodId);
+
+            target.hide();
+            $(".goddnumber-delete-" + goodId).show();
+            elem.attr("disabled", "disabled")
+            this.goods.push({ id: goodId, count: Number(elem.val()), price: 1 });//element.val -> element.text
+
+            let total = 0;
+            this.goods.forEach(item => {
+                total += item.count * item.price;
+            });
+
+            alert(total);
+        })
+
+        $(".goodnumber-delete").on("click", ev => {
+            const target = $(ev.target);
+            const goodId = target.data("goodid");
+
+            target.hide();
+            $(".goddnumber-add-" + goodId).show();
+            $(".goddnumber-input-" + goodId).removeAttr("disabled");
+            $(".goddnumber-input-" + goodId).val(0);
         })
     }
 }
